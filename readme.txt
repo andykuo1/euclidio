54e12590d2548ce3ee462304ec38dea7a44cf54f
Andrew <andykuo1supergreen@gmail.com>
2018-05-22 11:51:32 -0700

    Made euclidio open source
    Originally created in December 2016

---
+++ readme.txt
@@ -0,0 +1,29 @@
+TODO LIST ():
+ +Absolute value and norms for LaTex (resolved, use \left|{}\right|)
+ +Load the special characters, C,R,Q,Z,N (resolved, use \mathbb)
+ -Replace the words on the buttons with the associated images. Does not apply to Home, Proofs, Euclidean Plane, and Cartesian Plane.
+ -Images in the background of the title webpage will be different for each of the following categories:
+	-Home/About (default image)
+	-Proofs (math proof/lots of writing image)
+	-Euclidean Plane (engineering blueprint (straight edge and compass) without measurements [lime green] image)
+	-Cartesian Plane (engineering blueprint (straight edge and compass) with measurements [sunny yellow] image)
+ -Finish content up to Circles
+
+ Images:
+ - unravel circles
+ - parabolas
+ - hyperbolas
+ - ellipses
+ + line
+ + ray
+ + segment
+ + polygons
+
+ Features:
+  - Main Menu Overhaul
+  - Responsive (For Phone)
+  - Scaleable UI / Text
+  - Fade in on Scroll / Hover
+  - Drop Down Menus
+
+ Axioms of Incidence and Order
---
+++ res/js/main.js
@@ -0,0 +1,57 @@
+addListener('load', window, function() {
+	/**Refactor clearblock functions*/
+	var list = document.querySelectorAll(".ei-clearblock[data-src], .ei-clearblock-parallax[data-src]");
+
+	for (var i = 0; i < list.length; i++) {
+		var url = list[i].getAttribute('data-src');
+		list[i].style.backgroundImage = "url('" + url + "')";
+	}
+
+	/**Add website content*/
+	var addr = 'https://s.codetasty.com/andykuo1/euclidio/';
+
+	var title = document.getElementById('title');
+	var info = document.getElementById('info');
+
+	if (title) {
+		var e = document.createElement('h1');
+		e.innerHTML = "Forever United Geometry Awesomeness";
+		title.appendChild(e);
+	}
+
+	if (info) {
+		var e = document.createElement('p');
+		e.innerHTML = "Hello. I am copyrighted. Just ask my owner. :) Alright? Good. That should deter most of you. If you really want to know what is going on, please refer to Section 9 Article 2 for the proper legal notice on the official webpage of chubbycat.com. Thank you.";
+		info.appendChild(e);
+	}
+
+	/**Add navigation bar content*/
+	var navlist = document.getElementById('navbar-content');
+	navlist.appendChild(createListItemLink('Proofs', addr + '00_proofs/00_index.html', true));
+	navlist.appendChild(createListItemLink('Neutral Geometry', addr + '01_neutral_geometry/00_index.html', true));
+	navlist.appendChild(createListItemLink('Euclidean Geometry', addr + '02_euclidean_geometry/00_index.html', true));
+	navlist.appendChild(createListItemLink('Analytic Geometry', addr + '03_analytic_geometry/alt.html', true));
+	navlist.appendChild(createListItemLink('Inversive Geometry', addr + '04_inversive_geometry/alt.html', true));
+	navlist.appendChild(createListItemLink('Hyperbolic Geometry', addr + '05_hyperbolic_geometry/alt.html', true));
+
+
+	var sidelist = document.getElementById('sidebar-content');
+	sidelist.appendChild(createListItemHeadLink('Proofs', addr + '00_proofs/00_index.html'));
+	sidelist.appendChild(createListItemLink('Direct Proofs', addr + '00_proofs/01_direct_proofs.html'));
+	sidelist.appendChild(createListItemLink('Exhaustion', addr + '00_proofs/02_exhaustion.html'));
+	sidelist.appendChild(createListItemLink('Induction', addr + '00_proofs/03_induction.html'));
+	sidelist.appendChild(createListItemLink('Contradiction', addr + '00_proofs/04_contradiction.html'));
+	sidelist.appendChild(createListItemLink('Contrapositive', addr + '00_proofs/05_contrapositive.html'));
+	sidelist.appendChild(createListItemHeadLink('Neutral Geometry', addr + '01_euclidean_plane/00_index.html'));
+	sidelist.appendChild(createListItemLink('', addr + '01_euclidean_plane/01_affine_axioms.html'));
+	sidelist.appendChild(createListItemLink('Basic Theorems in Affine Geometry', addr + '01_euclidean_plane/02_affine_theorems.html'));
+	sidelist.appendChild(createListItemLink('Euclidean Postulates', addr + '01_euclidean_plane/03_euclidean_postulates.html'));
+	sidelist.appendChild(createListItemHeadLink('Triangles and Proportions', addr + ''));
+	sidelist.appendChild(createListItemHeadLink('Polygons', addr + ''));
+	sidelist.appendChild(createListItemHeadLink('Circles', addr + ''));
+	//sidelist.appendChild(createListItemHeadLink('Cartesian Plane', addr + ''));
+	//.appendChild(createListItemHeadLink('Trigonometric Functions', addr + ''));
+	//sidelist.appendChild(createListItemHeadLink('Trigonometric Laws', addr + ''));
+	//sidelist.appendChild(createListItemHeadLink('Hyperbolic Functions', addr + ''));
+	//sidelist.appendChild(createListItemHeadLink('Conic Sections', addr + ''));
+});
